1af6a9a504445ec1c0e8074ce442f523cbce353e
Gomsi Mittal <gomsimittal23@gmail.com>
2023-02-22 01:40:17 +0530

    backend url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async(e) => {
 
   loader(msgDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgpt-clone-89lg.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
